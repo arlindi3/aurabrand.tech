d4e898436ffd64639483abb968f46cf8084986ea
arlindi3 <draboarlind@gmail.com>
2025-07-12 11:03:53 +0200

    test

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Button = ({ styles }) => {
-  return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
-      Get Started
-    </button>
-  )
-}
+  const whatsappNumber = "3550673869970"; // Replace with your WhatsApp number
+  const whatsappUrl = `https://wa.me/${whatsappNumber}`;
 
-export default Button
+  return (
+    <a
+      href={whatsappUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] inline-block`}
+    >
+      Get Started
+    </a>
+  );
+};
+
+export default Button;
